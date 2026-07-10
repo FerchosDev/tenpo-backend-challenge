a81b575bd4ff315bf62bcf2f23f0358cc3f6f0ca
FerchosDev <al235247@edu.uaa.mx>
2026-07-09 20:44:39 -0600

    chore: initial spring boot project setup

---
+++ src/test/java/com/tenpo/challenge/CalculationApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tenpo.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class CalculationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
